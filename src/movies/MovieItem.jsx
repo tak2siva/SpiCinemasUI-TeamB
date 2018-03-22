71960e5425bfa2e2cb6f06920e1e94cc969e0235
Tanmay <tanmay.goyal2306@gmail.com>
2018-03-22 12:23:22 +0530

    [Priyanka/Tanmay] Story#1 Rendering default image

--- src/movies/MovieItem.jsx
+++ src/movies/MovieItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import defaultImage from './default_image.png';
 
 const MovieItem = ({name, slug, experiences}) => {
     const imageUrl = `https://img.spicinemas.in/resources/images/movies/${slug}/150x207.jpg`;
     return (
         <div className="col-md-2">
             <br/>
-            <div align="center">
-                <img alt={name} src={imageUrl}/>
+            <div align="center" >
+                <img alt={name} src={imageUrl} 
+                onError={(e) => {e.target.src=defaultImage}}/>
                 <h5>{name}</h5>
                 <h6>{experiences}</h6>
             </div>
